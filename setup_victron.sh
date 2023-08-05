4c0f3df21906cd28a013581cef224ab6a9ab5d20
mcfrojd <mcfrojd@gmail.com>
2023-08-05 15:25:10 +0200

    Added done and an initial clear

--- setup_victron.sh
+++ setup_victron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+clear
 echo "move into home directory of the user root."
 cd /home/root
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -10,6 +12,7 @@ clear
 echo "download the latest tailscale package."
 # ( Check latest version "arm" at https://pkgs.tailscale.com/stable/#static )
 curl -o tailscale_loc.tgz https://pkgs.tailscale.com/stable/tailscale_1.46.1_arm.tgz
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -17,6 +20,7 @@ clear
 
 echo "uncompress the package."
 tar -xvf tailscale_loc.tgz
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -24,6 +28,7 @@ clear
 
 echo "copy the nessesary files to /usr/bin."
 cp tailscale tailscaled /usr/bin/
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -31,6 +36,7 @@ clear
 
 echo "copy the nessesary file to /etc/init.d."
 cp /home/root/tailscale_on_venus_os/etc/init.d/tailscaled /etc/init.d/
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -38,6 +44,7 @@ clear
 
 echo "make init script executable"
 chmod +x /etc/init.d/tailscaled
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -47,6 +54,7 @@ echo "test the init script"
 /etc/init.d/tailscaled start
 /etc/init.d/tailscaled stop
 /etc/init.d/tailscaled start
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -54,6 +62,7 @@ clear
 
 echo "configure tailscale init script to start automatically on boot"
 update-rc.d tailscaled defaults
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
@@ -61,6 +70,7 @@ clear
 
 echo "enable ip-forward"
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf sudo sysctl -p /etc/sysctl.conf
+echo "done."
 sleep 1
 echo "press any key to continue"
 read -s -n 1
