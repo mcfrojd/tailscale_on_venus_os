4d5be37966413c5263b382ffb22897f4af967743
mcfrojd <mcfrojd@gmail.com>
2023-08-05 15:37:15 +0200

    Added commenta

--- setup_victron.sh
+++ setup_victron.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 clear
+
+#
+# move into home directory of the user root.
+#
 echo "move into home directory of the user root."
 echo ""
 cd /home/root
@@ -12,9 +16,11 @@ echo ""
 read -s -n 1
 clear
 
+#
+# download the latest tailscale package.
+#
 echo "download the latest tailscale package."
 echo ""
-# ( Check latest version "arm" at https://pkgs.tailscale.com/stable/#static )
 curl -o tailscale_loc.tgz https://pkgs.tailscale.com/stable/tailscale_1.46.1_arm.tgz
 echo "done."
 echo ""
@@ -24,6 +30,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# uncompress the package.
+#
 echo "uncompress the package."
 echo ""
 tar -xvf tailscale_loc.tgz
@@ -35,6 +44,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# copy the nessesary files to /usr/bin.
+#
 echo "copy the nessesary files to /usr/bin."
 echo ""
 cp tailscale tailscaled /usr/bin/
@@ -46,6 +58,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# copy the nessesary file to /etc/init.d.
+#
 echo "copy the nessesary file to /etc/init.d."
 echo ""
 cp /home/root/tailscale_on_venus_os/etc/init.d/tailscaled /etc/init.d/
@@ -57,6 +72,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# make init script executable
+#
 echo "make init script executable"
 echo ""
 chmod +x /etc/init.d/tailscaled
@@ -68,6 +86,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# test the init script
+#
 echo "test the init script"
 echo ""
 /etc/init.d/tailscaled start
@@ -81,6 +102,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# configure tailscale init script to start automatically on boot
+#
 echo "configure tailscale init script to start automatically on boot"
 echo ""
 update-rc.d tailscaled defaults
@@ -91,6 +115,9 @@ echo ""
 read -s -n 1
 clear
 
+#
+# enable ip-forward
+#
 echo "enable ip-forward"
 echo ""
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf sudo sysctl -p /etc/sysctl.conf
@@ -102,9 +129,15 @@ echo ""
 read -s -n 1
 clear
 
+#
+# connect to your tailscale account
+#
 echo "connect to your tailscale account"
 echo ""
 tailscale up --advertise-routes=192.168.77.0/24
 
+#
+# if there was no errors, tailscale should be installed and ready!
+#
 echo "if there was no errors, tailscale should be installed and ready!"
 echo ""
