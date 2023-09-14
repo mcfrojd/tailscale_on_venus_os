6db744233a66705892a2f640bbefcfaa358b05c2
mcfrojd <mcfrojd@gmail.com>
2023-09-14 15:00:32 +0200

    added pipe

--- setup_victron.sh
+++ setup_victron.sh
@@ -125,7 +125,7 @@ clear
 #
 echo "enable ip-forward"
 echo ""
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf sudo sysctl -p /etc/sysctl.conf
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf | sudo sysctl -p /etc/sysctl.conf
 echo "done."
 echo ""
 sleep 1
